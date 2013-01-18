1a0f8e0a142ad45acc7f217a7faac9f1ca9c8edb
Manuel Rivera <themanuelrc@gmail.com>
2013-01-18 12:26:18 -0500

    Cambios de texto
    - Cambiado texto de COMO LO HACEMOS
    - La barra VERDE de LA WEB ES EL CAMINO la hemos borrado

---
+++ config.rb
@@ -0,0 +1,6 @@
+#config.rb
+http_path = "/"
+css_dir = "css" #Carpeta en donde iran los .css
+sass_dir = "sass" #carpeta donde iran los .scss
+images_dir = "img" #carpeta en donde van las imagenes
+output_style = :expanded # :expanded, :nested, :compact, :compressed
